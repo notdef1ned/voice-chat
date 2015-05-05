f506cf6aeec6a83672b20441a9cdf8b580d56f03
not_def1ned <n0td3f1n3d@gmail.com>
2015-05-04 20:06:04 -0700

    previous commit fix

---
+++ VoiceChat/BaseControls/SettingsTabControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BaseControls
+{
+    public class SettingsTabControl : TabControl
+    {
+        public SettingsTabControl()
+        {
+            SetStyle();
+        }
+
+        public void SetStyle()
+        {
+           //Resources.Source = new Uri("/BaseControls;component/Themes/Generic.xaml", UriKind.RelativeOrAbsolute);
+           //Style = (Style)FindResource("TabControlStyle");
+        }
+
+    }
+}
