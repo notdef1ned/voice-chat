503e8a9abd1ce3db0e01be8b3b65ac0d1566bf76
not_def1ned <n0td3f1n3d@gmail.com>
2015-05-12 13:25:39 -0700

    ChatButton style implemented

---
+++ VoiceChat/BaseControls/ChatButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BaseControls
+{
+    public class ChatButton : Button
+    {
+
+        public ChatButton()
+        {
+            MaxHeight = MinHeight = 25;
+            SetTabStyle();
+        }
+
+        protected void SetTabStyle()
+        {
+            Resources.Source = new Uri("/BaseControls;component/Themes/Generic.xaml", UriKind.RelativeOrAbsolute);
+            Style = (Style)FindResource("ChatButtonStyle");
+        }
+    }
+
+
+}
